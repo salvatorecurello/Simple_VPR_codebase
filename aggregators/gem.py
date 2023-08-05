c0ba5690a5de02e76ea06a740debc83ba978b76c
Salvatore Curello <118004144+salvatorecurello@users.noreply.github.com>
2023-08-05 12:21:06 +0200

    update gem

--- aggregators/gem.py
+++ aggregators/gem.py
@@ -1,14 +1,15 @@
 import torch
-import torch.nn as nn
-from torch.nn.parameter import Parameter
 import torch.nn.functional as F
+import torch.nn as nn
 
+class GeMPool(nn.Module):
 
-class GeM(nn.Module):
     def __init__(self, p=3, eps=1e-6):
-        super(GeM, self).__init__()
-        self.p = Parameter(torch.ones(1) * p)
+        super().__init__()
+        self.p = nn.Parameter(torch.ones(1)*p)
         self.eps = eps
 
     def forward(self, x):
-        return F.avg_pool2d(x.clamp(min=self.eps).pow(self.p), (x.size(-2), x.size(-1))).pow(1.0 / self.p)
+        x = F.avg_pool2d(x.clamp(min=self.eps).pow(self.p), (x.size(-2), x.size(-1))).pow(1./self.p)
+        x = x.flatten(1)
+        return F.normalize(x, p=2, dim=1)
