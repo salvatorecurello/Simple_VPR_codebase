9ea0baf421e812d5663d295404f99551876addc9
Antonio Cirigliano <124920752+antoniocirigliano@users.noreply.github.com>
2025-09-18 16:27:31 +0200

    Add files via upload

---
+++ gem.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+class GeMPool(nn.Module):
+
+    def __init__(self, p=3, eps=1e-6):
+        super().__init__()
+        self.p = nn.Parameter(torch.ones(1)*p)
+        self.eps = eps
+
+    def forward(self, x):
+        x = F.avg_pool2d(x.clamp(min=self.eps).pow(self.p), (x.size(-2), x.size(-1))).pow(1./self.p)
+        x = x.flatten(1)
+        return F.normalize(x, p=2, dim=1)
